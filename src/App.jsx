6672d704ad4b951fa0f6dbf235155bfa9f1fd688
Tom Dossman <tdossman.development@gmail.com>
2024-02-26 18:31:02 -0500

    Basic Footer Created w/links to github and linkedin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <Header />
+
+      <Footer />
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,26 @@
+import githubIcon from '../assets/media/githubIcon.png'
 
-export default function Footer(){
+export default function Footer() {
 
   return (
     <>
-    
-    // design code here
+
+      <footer className="container-fluid d-flex align-items-center justify-content-around">
+
+
+        <a className="nav-link" href="https://github.com/Dossman-thomas" target="_blank">
+          <img src={githubIcon} alt="GitHub Icon" />
+        </a>
+
+        <a className="nav-link" href="https://www.linkedin.com/in/tom-dossman-66b4092a5/" target="_blank">
+          LinkedIn
+        </a>
+
+        {/* <a className="nav-link" href="#">
+          Contact
+        </a> */}
+
+      </footer>
 
     </>
 
