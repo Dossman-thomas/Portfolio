a6c8d5970440a6deaabfe093b18506485d5c8438
Tom Dossman <tdossman.development@gmail.com>
2024-02-26 14:00:25 -0500

    Basic header w/nav bar and some initial styling to put things in place

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,36 @@
 
-export default function Header(){
+export default function Header() {
 
   return (
     <>
-    
-    // design code here
+
+      <header className="container-fluid header d-flex align-items-center">
+
+        <nav className="navbar d-flex justify-content-between align-items-center">
+
+            <a className="navbar-brand ms-4" href="#">
+              <h1>Tom Dossman</h1>
+            </a>
+
+            <div className="nav-box d-flex align-items-center gap-4 me-4">
+
+              <a className="nav-link" href="#">
+                Portfolio
+              </a>
+
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+
+              <a className="nav-link" href="#">
+                Resume
+              </a>
+
+            </div>
+
+        </nav>
+
+      </header>
 
     </>
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <App />
 );
