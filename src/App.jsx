a1d3b3b9b1d04d2c0597ccb92316338e25291df3
Tom Dossman <tdossman.development@gmail.com>
2024-02-27 14:06:55 -0500

    about me section created w/greeting and bio section. Still needs recent photo of dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import './App.css'
+
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import AboutMe from './components/AboutMe.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,7 @@ function App() {
   return (
     <>
       <Header />
-
+      <AboutMe />
       <Footer />
     </>
   )
