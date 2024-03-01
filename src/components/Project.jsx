386af8f2948cd5d96a7a3f24c8cafd92620452e0
Tom Dossman <tdossman.development@gmail.com>
2024-02-29 21:46:09 -0500

    half stylized cards for projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,42 +1,46 @@
+import '../../public/css/project.css'
 
 export default function Project(props){
 
   return (
     <>
     
-    <div className="card">
+      <div className="card">
 
-      <div className="card-img-top position-relative overflow-hidden border rounded">
+        <div className="card-img-top position-relative overflow-hidden border rounded">
 
-        <img 
-          src={props.imageUrl} 
-          alt={props.description} className="w-100" 
-        />
+          <img 
+            src={props.imageUrl} 
+            alt={props.description} className="w-100" 
+          />
 
-        <div className="overlay d-flex flex-column align-items-center justify-content-center">
+          <div className="overlay d-flex flex-column align-items-center justify-content-center">
 
-          <h3 className="text-white">
-            {props.title}
-          </h3>
+            <h3 className="text-white">
+              {props.title}
+            </h3>
 
-          <p className="text-white">
-            {props.description}
-          </p>
+            <p className="text-white">
+              {props.description}
+            </p>
 
-          <div className="links mt-2">
+            <div className="links mt-2">
 
-            <a href={props.deployedLink} target="_blank" rel="noopener noreferrer" className="text-white">
-              View Site
-            </a>
+              <a href={props.deployedLink} target="_blank" rel="noopener noreferrer" className="text-white">
+                View Site
+              </a>
 
-            <a href={props.githubRepo} target="_blank" rel="noopener noreferrer" className="text-white">
-              GitHub Repo
-            </a>
+              <a href={props.githubRepo} target="_blank" rel="noopener noreferrer" className="text-white">
+                GitHub Repo
+              </a>
+
+            </div>
 
           </div>
+
         </div>
+        
       </div>
-    </div>
 
     </>
 
