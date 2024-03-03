55090e22d96dee0733acb4a7fcb3750ea1609fbb
Tom Dossman <tdossman.development@gmail.com>
2024-03-03 11:52:05 -0500

    React router configured to include routes for the index, portfolio, resume, and contact pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx';
+
+// import css framework and files
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
+// import pages the router will use to conditionally show the appropriate views
+import App from './App.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+import Contact from './pages/Contact.jsx';
+
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <RouterProvider router={router} />
 );
