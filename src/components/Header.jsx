3625dcfd0a8aa0bcd16685dabc0e7c6ab85a4b1f
Tom Dossman <tdossman.development@gmail.com>
2024-03-09 17:13:26 -0500

    highlighting of current page in nav bar configured although there is a slight bug where you have to click on the page for the link to highlight.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,70 +1,37 @@
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const location = useLocation();
+  const [currentPage, setCurrentPage] = useState(location.pathname);
+
+  useEffect(() => {
+    setCurrentPage(location.pathname);
+  }, [location.pathname]);
+
+  const activeLinkStyle = {
+    color: '#C12121',
+    // Add any other styles you want for the active link
+  };
 
   return (
-    <>
-
-      <header className="container-fluid header d-flex align-items-center">
-
-        <nav className="navbar d-flex justify-content-between align-items-center">
-
-
-          <Link
-            to={'/'}
-            className='navbar-brand ms-4'
-          >
-
-            <h1 id="dev-name">
-              Tom Dossman
-            </h1>
-
-          </Link>
-
-          <div className="nav-box d-flex align-items-center gap-4 me-4">
-
-            <Link
-              to={'/portfolio'}
-              className='nav-link'
-            >
-
-              <p className="text">
-                Portfolio
-              </p>
-
-            </Link>
-
-            <Link
-              to={'/resume'}
-              className='nav-link'
-            >
-
-              <p className="text">
-                Resume
-              </p>
-
-            </Link>
-
-            <Link
-              to={'/contact'}
-              className='nav-link'
-            >
-
-              <p className="text">
-                Contact
-              </p>
-
-            </Link>
-
-
-          </div>
-
-        </nav>
-
-      </header>
-
-    </>
-
-  )
-
+    <header className="container-fluid header d-flex align-items-center">
+      <nav className="navbar d-flex justify-content-between align-items-center">
+        <NavLink to={'/'} className="navbar-brand ms-4 text" activeClassName="active" activeStyle={activeLinkStyle}>
+          <h1 id="dev-name">Tom Dossman</h1>
+        </NavLink>
+        <div className="nav-box d-flex align-items-center gap-4 me-4">
+          <NavLink to={'/portfolio'} className="nav-link" activeClassName="active" activeStyle={activeLinkStyle}>
+            <p className="text">Portfolio</p>
+          </NavLink>
+          <NavLink to={'/resume'} className="nav-link" activeClassName="active" activeStyle={activeLinkStyle}>
+            <p className="text">Resume</p>
+          </NavLink>
+          <NavLink to={'/contact'} className="nav-link" activeClassName="active" activeStyle={activeLinkStyle}>
+            <p className="text">Contact</p>
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
 }
